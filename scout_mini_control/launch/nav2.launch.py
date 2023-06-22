5996cc2dbe5309d1fcfcbbd9e35fd409df1eb6a4
Liam <liamd@indrorobotics.com>
2023-06-22 17:09:58 -0400

    Working slam_toolbox

--- scout_mini_control/launch/nav2.launch.py
+++ scout_mini_control/launch/nav2.launch.py
@@ -21,38 +21,38 @@ from launch.actions import (
 
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    namespace = LaunchConfiguration('namespace')
+
+    
+
+    scout_mini_control_pkg = get_package_share_directory('scout_mini_control')
+    slam_toolbox_pkg = get_package_share_directory('slam_toolbox')
+
+    slam_params_file = DeclareLaunchArgument(
+        'slam_params_file', default_value=str(os.path.join(scout_mini_control_pkg, 'config', 'slam.yaml')))
+    
+    declare_slam_params_file_cmd = DeclareLaunchArgument(
+        'slam_params_file',
+        default_value=os.path.join(get_package_share_directory("scout_mini_control"),
+                                   'config', 'slam.yaml'),
+        description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
+
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation/Gazebo clock')
+    
+    declare_namespace_argument = DeclareLaunchArgument('namespace', default_value='scout_mini',
+        description='Top-level namespace')
 
     ld = LaunchDescription()
 
-    scout_mini_control_pkg = get_package_share_directory('scout_mini_control')
-
-    sim_time_argument = DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                              description='Flag to enable use_sim_time')
-
-    nav2_bringup_launch = ExecuteProcess(
-        name="launch_navigation",
-        cmd=[
-            "ros2",
-             "launch",
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("nav2_bringup"),
-                    "launch",
-                    "navigation_launch.py",
-                ]
-            ),
-            "use_sim_time:=False",
-            str("params_file:=" +
-                str(os.path.join(scout_mini_control_pkg, 'config', 'nav2.yaml')))
-        ],
-        output="screen"
-
-    )
     depthimage_to_laserscan_launch = Node(
         package='depthimage_to_laserscan', executable='depthimage_to_laserscan_node',
         remappings=[('depth', '/scout_mini/zed_depth_camera/depth/image_raw'),
                     ('depth_camera_info', '/scout_mini/zed_depth_camera/depth/camera_info'),
-                    ('scan', 'scout_mini/scan')],
+                    ('scan', 'scan')],
         parameters=[{
                 'range_min' : 0.2,
                 'range_max' : 20.0,
@@ -62,29 +62,49 @@ def generate_launch_description():
         name='depthimage_to_laserscan'
     )
 
-    nav2_bringup_launch = ExecuteProcess(
-        name="launch_navigation",
-        cmd=[
-            "ros2",
-            "launch",
-            PathJoinSubstitution(
-                [
-                    FindPackageShare("nav2_bringup"),
-                    "launch",
-                    "navigation_launch.py",
-                ]
-            ),
-            "use_sim_time:=True",
-            str("params_file:=" + str(os.path.join(scout_mini_control_pkg, 'config', 'nav2.yaml')))
-        ],
-        output="screen"
 
+    # slam_toolbox_node = Node(
+    #     package='slam_toolbox',
+    #     executable='async_slam_toolbox_node',
+    #     name='slam_toolbox',
+    #     namespace='scout_mini',
+    #     output='screen',
+    #     parameters=[
+    #         {'slam_params_file' : str(os.path.join(get_package_share_directory("scout_mini_control"),
+    #                                'config', 'slam.yaml'))},
+    #         {'use_sim_time' : use_sim_time}
+    #     ]
+    # )
+
+    slam_toolbox_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [slam_toolbox_pkg, '/launch/online_async_launch.py']),
     )
+    nav2_bringup_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('nav2_bringup'),
+                'launch', 'navigation_launch.py',
+            ])
+        ]),
+        launch_arguments={
+            'namespace': 'scout_mini',
+            'use_sim_time' : 'true',
+            'params_file' : str(os.path.join(scout_mini_control_pkg, 'config', 'nav2.yaml')),
+            # 'container_name' : 'scout_mini',
 
+        }.items(),
+    )
+    # Launch Arguments
+    ld.add_action(declare_slam_params_file_cmd)
+    ld.add_action(declare_use_sim_time_argument)
+    ld.add_action(declare_namespace_argument)
+    ld.add_action(slam_params_file)
     # Navigation Nodes
-    ld.add_action(nav2_bringup_launch)
+    #ld.add_action(slam_toolbox_launch)
+    ld.add_action(slam_toolbox_launch)
+    #ld.add_action(nav2_bringup_launch)
     ld.add_action(depthimage_to_laserscan_launch)
 
-    ld.add_action(sim_time_argument)
 
     return ld
