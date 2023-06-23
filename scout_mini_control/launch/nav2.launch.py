2a725e02d4d553ad12cf5ace1782cdd8a969e019
Liam <liamd@indrorobotics.com>
2023-06-23 10:41:17 -0400

    NAV2 Bringup functioning - Gazebo fix

--- scout_mini_control/launch/nav2.launch.py
+++ scout_mini_control/launch/nav2.launch.py
@@ -21,60 +21,59 @@ from launch.actions import (
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    namespace = LaunchConfiguration('namespace')
 
-    
-
+    # Package Share Directories
     scout_mini_control_pkg = get_package_share_directory('scout_mini_control')
     slam_toolbox_pkg = get_package_share_directory('slam_toolbox')
 
-    slam_params_file = DeclareLaunchArgument(
-        'slam_params_file', default_value=str(os.path.join(scout_mini_control_pkg, 'config', 'slam.yaml')))
-    
+    # Launch Configurations
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    namespace = LaunchConfiguration('namespace')
+    slam_params_file = LaunchConfiguration('slam_params_file')
+
+    #Declaring Arguments
+    declare_use_sim_time_argument = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation/Gazebo clock')
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
         default_value=os.path.join(get_package_share_directory("scout_mini_control"),
                                    'config', 'slam.yaml'),
         description='Full path to the ROS2 parameters file to use for the slam_toolbox node')
 
-    declare_use_sim_time_argument = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value='true',
-        description='Use simulation/Gazebo clock')
-    
     declare_namespace_argument = DeclareLaunchArgument('namespace', default_value='scout_mini',
         description='Top-level namespace')
+    
+    declare_use_namespace_argument = DeclareLaunchArgument('use_namespace', default_value='True', description='Whether or not to use the namespace argument')
 
-    ld = LaunchDescription()
+    # Launching Nodes
+    start_async_slam_toolbox_node = Node(
+        parameters=[
+          slam_params_file,
+          {'use_sim_time': use_sim_time}
+        ],
+        package='slam_toolbox',
+        executable='async_slam_toolbox_node',
+        name='slam_toolbox',
+        namespace='scout_mini',
+        output='screen')
 
     depthimage_to_laserscan_launch = Node(
         package='depthimage_to_laserscan', executable='depthimage_to_laserscan_node',
         remappings=[('depth', '/scout_mini/zed_depth_camera/depth/image_raw'),
                     ('depth_camera_info', '/scout_mini/zed_depth_camera/depth/camera_info'),
-                    ('scan', 'scan')],
+                    ('scan', 'scout_mini/scan')],
         parameters=[{
                 'range_min' : 0.2,
-                'range_max' : 20.0,
+                'scan_height' : 5,
+                'range_max' : 10.0,
                 'scan_time' : 0.0333333,
                 'output_frame' : 'zed2_right_camera_frame',
         }],
         name='depthimage_to_laserscan'
     )
-
-
-    # slam_toolbox_node = Node(
-    #     package='slam_toolbox',
-    #     executable='async_slam_toolbox_node',
-    #     name='slam_toolbox',
-    #     namespace='scout_mini',
-    #     output='screen',
-    #     parameters=[
-    #         {'slam_params_file' : str(os.path.join(get_package_share_directory("scout_mini_control"),
-    #                                'config', 'slam.yaml'))},
-    #         {'use_sim_time' : use_sim_time}
-    #     ]
-    # )
+    
 
     slam_toolbox_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -88,22 +87,27 @@ def generate_launch_description():
             ])
         ]),
         launch_arguments={
-            'namespace': 'scout_mini',
+            # 'namespace': 'scout_mini',
             'use_sim_time' : 'true',
             'params_file' : str(os.path.join(scout_mini_control_pkg, 'config', 'nav2.yaml')),
             # 'container_name' : 'scout_mini',
 
         }.items(),
     )
+
+    ld = LaunchDescription()
     # Launch Arguments
-    ld.add_action(declare_slam_params_file_cmd)
     ld.add_action(declare_use_sim_time_argument)
-    ld.add_action(declare_namespace_argument)
-    ld.add_action(slam_params_file)
+    ld.add_action(declare_slam_params_file_cmd)
+    
+    #ld.add_action(declare_namespace_argument) #NOT WORKING IN THE CURRENT NAV STACK -- GET FAILURE TO LOAD CRITICS
+    #ld.add_action(declare_use_namespace_argument)
     # Navigation Nodes
+    ld.add_action(start_async_slam_toolbox_node)
+
+
     #ld.add_action(slam_toolbox_launch)
-    ld.add_action(slam_toolbox_launch)
-    #ld.add_action(nav2_bringup_launch)
+    ld.add_action(nav2_bringup_launch)
     ld.add_action(depthimage_to_laserscan_launch)
 
 
